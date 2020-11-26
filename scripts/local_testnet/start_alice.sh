1c942f14a75da98b36c3c235416d52d927bafc15
parall4x <parallax@bittensor.com>
2020-11-26 10:28:46 -0500

    Added config generation script, as well as the right configs to make a PoA network work

--- scripts/local_testnet/start_alice.sh
+++ scripts/local_testnet/start_alice.sh
@@ -6,6 +6,5 @@
 --ws-port 9944 \
 --rpc-port 9933 \
 --node-key 0000000000000000000000000000000000000000000000000000000000000001 \
---telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
 --rpc-methods=Unsafe \
 
--- scripts/local_testnet/start_charlie.sh
+++ scripts/local_testnet/start_charlie.sh
@@ -6,7 +6,6 @@
 --ws-port 9947 \
 --rpc-port 9935 \
 --node-key 0000000000000000000000000000000000000000000000000000000000000003 \
---bootnodes /ip4/127.0.0.1/tcp/30333/p2p/$1 \
---telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
+--bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp \
 --rpc-methods=Unsafe \
 
