899ce50612f32fc21f1834a7180497b5614bd166
Parallax <parall4x@users.noreply.github.com>
2020-12-21 11:21:12 -0500

    [#176236216] - Split remove_stake off into submodule

--- pallets/subtensor/src/staking.rs
+++ pallets/subtensor/src/staking.rs
@@ -60,4 +60,54 @@ impl<T: Trait> Module<T> {
         debug::info!("--- Done add_stake.");
         Ok(())
     }
+
+
+    pub fn do_remove_stake(origin : T::Origin, hotkey : T::AccountId, ammount_unstaked : u32) -> dispatch::DispatchResult
+    {
+        // ---- We check the transaction is signed by the caller
+        // and retrieve the T::AccountId pubkey information.
+        let caller = ensure_signed(origin)?;
+        debug::info!("--- Called remove_stake with {:?}, hotkey {:?} and ammount {:?}", caller, hotkey, ammount_unstaked);
+
+        // ---- We query the Neuron set for the NeuronMetadata stored under
+        // the passed hotkey.
+        ensure!(Neurons::<T>::contains_key(&hotkey), Error::<T>::NotActive);
+        let neuron: NeuronMetadataOf<T> = Neurons::<T>::get(&hotkey);
+        debug::info!("Got metadata for hotkey.");
+
+        // ---- We check that the NeuronMetadata is linked to the calling
+        // cold key, otherwise throw a NonAssociatedColdKey error.
+        ensure!(neuron.coldkey == caller, Error::<T>::NonAssociatedColdKey);
+
+        // --- We call the emit function for the associated hotkey.
+        // Neurons must call an emit before they remove
+        // stake or they may be able to cheat their peers of inflation.
+        Self::emit( &hotkey );
+
+        // ---- We check that the hotkey has enough stake to withdraw
+        // and then withdraw from the account.
+        let hotkey_stake: u32 = Stake::<T>::get(&hotkey);
+        ensure!(hotkey_stake >= ammount_unstaked, Error::<T>::NotEnoughStaketoWithdraw);
+        Stake::<T>::insert(&hotkey, hotkey_stake - ammount_unstaked);
+        debug::info!("Withdraw: {:?} from hotkey staking account for new ammount {:?} staked", ammount_unstaked, hotkey_stake - ammount_unstaked);
+
+        // --- We perform the withdrawl by converting the stake to a u32 balance
+        // and deposit the balance into the coldkey account. If the coldkey account
+        // does not exist it is created.
+        // TODO(const): change to u32
+        let _ = T::Currency::deposit_creating(&caller, Self::u32_to_balance(ammount_unstaked));
+        debug::info!("Deposit {:?} into coldkey balance ", Self::u32_to_balance(ammount_unstaked));
+
+        // --- We update the total staking pool with the removed funds.
+        let total_stake: u32 = TotalStake::get();
+        TotalStake::put(total_stake - ammount_unstaked);
+        debug::info!("Remove {:?} from total stake, now {:?} ", ammount_unstaked, TotalStake::get());
+
+        // ---- Emit the unstaking event.
+        Self::deposit_event(RawEvent::StakeRemoved(hotkey, ammount_unstaked));
+        debug::info!("--- Done remove_stake.");
+
+        // --- Done and ok.
+        Ok(())
+    }
 }
