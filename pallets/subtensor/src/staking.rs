8b5ccd82b4813739ef63db274276ecce95587f79
Parallax <parall4x@users.noreply.github.com>
2020-12-28 14:44:20 -0500

    [#176299292] - Refactoed do_remove_stake

--- pallets/subtensor/src/staking.rs
+++ pallets/subtensor/src/staking.rs
@@ -14,12 +14,11 @@ impl<T: Trait> Module<T> {
 
         // Check if the hotkey is active
         ensure!(Self::is_active(&hotkey), Error::<T>::NotActive);
-
         let neuron = Self::get_neuron_for_hotkey(&hotkey);
 
         // ---- We check that the NeuronMetadata is linked to the calling
         // cold key, otherwise throw a NonAssociatedColdKey error.
-        ensure!(Self::does_neuron_belong_to_coldkey(&neuron, &coldkey), Error::<T>::NonAssociatedColdKey);
+        ensure!(Self::neuron_belongs_to_coldkey(&neuron, &coldkey), Error::<T>::NonAssociatedColdKey);
 
         // --- We call the emit function for the associated hotkey. Neurons must call an emit before they change 
         // their stake or else can cheat the system by adding stake just before
@@ -31,10 +30,10 @@ impl<T: Trait> Module<T> {
         // create the staking transaction.
         let stake_as_balance = Self::u64_to_balance( stake_to_be_added );
         ensure!(stake_as_balance.is_some(), Error::<T>::CouldNotConvertToBalance);
-        let stake_as_balance = stake_as_balance.unwrap();
+        // let stake_as_balance = stake_as_balance.unwrap();
 
         ensure!(Self::coldkey_has_enough_balance(&coldkey, &stake_as_balance), Error::<T>::NotEnoughBalanceToStake);
-        Self::withdraw_stake_from_coldkey_account(&coldkey, &stake_as_balance);
+        Self::remove_stake_from_coldkey_account(&coldkey, &stake_as_balance);
         Self::add_stake_to_neuron_hotkey_account(neuron, stake_to_be_added);
         Self::increase_total_stake(stake_to_be_added);
 
@@ -47,11 +46,62 @@ impl<T: Trait> Module<T> {
         Ok(())
     }
 
-    fn coldkey_has_enough_balance(coldkey: &T::AccountId, staked_currency: &<T::Currency as Currency<<T as system::Trait>::AccountId>>::Balance) -> bool {
-        let new_potential_balance = T::Currency::free_balance(&coldkey) - *staked_currency;
-        let can_withdraw = T::Currency::ensure_can_withdraw(&coldkey, *staked_currency, WithdrawReasons::except(WithdrawReason::Tip), new_potential_balance).is_ok();
-        can_withdraw
+
+
+    pub fn do_remove_stake(origin: T::Origin, hotkey: T::AccountId, stake_to_be_removed: u64) -> dispatch::DispatchResult {
+        
+        // ---- We check the transaction is signed by the caller
+        // and retrieve the T::AccountId pubkey information.
+        let coldkey = ensure_signed(origin)?;
+        debug::info!("--- Called remove_stake with {:?}, hotkey {:?} and ammount {:?}", coldkey, hotkey, stake_to_be_removed);
+
+        // ---- We query the Neuron set for the NeuronMetadata stored under
+        // the passed hotkey.
+        ensure!(Self::is_active(&hotkey), Error::<T>::NotActive);
+        let neuron = Self::get_neuron_for_hotkey(&hotkey);
+
+        // ---- We check that the NeuronMetadata is linked to the calling
+        // cold key, otherwise throw a NonAssociatedColdKey error.
+        ensure!(Self::neuron_belongs_to_coldkey(&neuron, &coldkey), Error::<T>::NonAssociatedColdKey);
+
+        // --- We call the emit function for the associated hotkey.
+        // Neurons must call an emit before they remove
+        // stake or they may be able to cheat their peers of inflation.
+        Self::emit_from_uid( neuron.uid );
+
+        // ---- We check that the hotkey has enough stake to withdraw
+        // and then withdraw from the account.
+        // let hotkey_stake: u64 = Stake::get( neuron.uid );
+        ensure!(Self::has_enough_stake(&neuron, stake_to_be_removed), Error::<T>::NotEnoughStaketoWithdraw);
+
+        let stake_to_be_added_as_currency = Self::u64_to_balance(stake_to_be_removed);
+        ensure!(stake_to_be_added_as_currency.is_some(), Error::<T>::CouldNotConvertToBalance);
+
+        // --- We perform the withdrawl by converting the stake to a u64 balance
+        // and deposit the balance into the coldkey account. If the coldkey account
+        // does not exist it is created.
+        Self::add_stake_to_coldkey_account(&coldkey, &stake_to_be_added_as_currency);
+        Self::remove_stake_from_neuron_hotkey_account(neuron, stake_to_be_removed);
+        Self::reduce_total_stake(stake_to_be_removed);
+
+        // ---- Emit the unstaking event.
+        Self::deposit_event(RawEvent::StakeRemoved(hotkey, stake_to_be_removed));
+        debug::info!("--- Done remove_stake.");
+        
+        // --- Done and ok.
+        Ok(())
     }
+
+
+
+
+    /********************************
+    --==[[  Helper functions   ]]==--
+    *********************************/
+
+
+
+
 
     fn increase_total_stake(increment: u64) {
         // --- We update the total staking pool with the new funds.
@@ -60,16 +110,35 @@ impl<T: Trait> Module<T> {
         debug::info!("Added {:?} to total stake, now {:?}", increment, TotalStake::get());
     }
 
+    fn reduce_total_stake(decrement: u64) {
+        // --- We update the total staking pool with the removed funds.
+        let total_stake: u64 = TotalStake::get();
+        TotalStake::put(total_stake - decrement);
+        debug::info!("Remove {:?} from total stake, now {:?} ", decrement, TotalStake::get());
+    }
+
     fn add_stake_to_neuron_hotkey_account(neuron: NeuronMetadataOf<T>, amount: u64) {
         let hotkey_stake: u64 = Stake::get(neuron.uid);
         Stake::insert(neuron.uid, hotkey_stake + amount);
         debug::info!("Added new stake: {:?} to uid {:?}", amount, neuron.uid);
     }
 
-    fn withdraw_stake_from_coldkey_account(coldkey: &T::AccountId, amount: &<T::Currency as Currency<T::AccountId>>::Balance) {
+    fn remove_stake_from_neuron_hotkey_account(neuron: NeuronMetadataOf<T>, amount: u64) {
+        let hotkey_stake: u64 = Stake::get(neuron.uid);
+        Stake::insert(neuron.uid, hotkey_stake - amount);
+        debug::info!("Withdraw: {:?} from hotkey staking account for new ammount {:?} staked", amount, hotkey_stake - amount);
+    }
+
+    fn add_stake_to_coldkey_account(coldkey: &T::AccountId, amount: &Option<<<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance>) {
+        //@todo (Parallax, 28-12-2020) implement error handling
+        let _ = T::Currency::deposit_creating(&coldkey, amount.unwrap());
+        debug::info!("Deposit {:?} into coldkey balance ", amount.unwrap());
+    }
+
+    fn remove_stake_from_coldkey_account(coldkey: &T::AccountId, amount: &Option<<<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance>) {
         // @Todo (parallax, 28-12-2020) Do error handling on this call
-        let _ = T::Currency::withdraw(&coldkey, *amount, WithdrawReasons::except(WithdrawReason::Tip), ExistenceRequirement::KeepAlive);
-        debug::info!("Withdrew {:?} from coldkey: {:?}", *amount, coldkey);
+        let _ = T::Currency::withdraw(&coldkey, amount.unwrap(), WithdrawReasons::except(WithdrawReason::Tip), ExistenceRequirement::KeepAlive);
+        debug::info!("Withdrew {:?} from coldkey: {:?}", amount.unwrap(), coldkey);
     }
 
     // ---- We query the Neuron set for the neuron data stored under
@@ -84,58 +153,19 @@ impl<T: Trait> Module<T> {
         return Neurons::<T>::contains_key(&hotkey);
     }
 
-    fn does_neuron_belong_to_coldkey(neuron : &NeuronMetadataOf<T>, coldkey : &T::AccountId) -> bool {
+    fn neuron_belongs_to_coldkey(neuron : &NeuronMetadataOf<T>, coldkey : &T::AccountId) -> bool {
         return neuron.coldkey == *coldkey
     }
 
+    fn coldkey_has_enough_balance(coldkey: &T::AccountId, amount: &Option<<<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance>) -> bool {
+        let new_potential_balance = T::Currency::free_balance(&coldkey) - amount.unwrap();
+        let can_withdraw = T::Currency::ensure_can_withdraw(&coldkey, amount.unwrap(), WithdrawReasons::except(WithdrawReason::Tip), new_potential_balance).is_ok();
+        can_withdraw
+    }
 
-    pub fn do_remove_stake(origin: T::Origin, hotkey: T::AccountId, ammount_unstaked: u64) -> dispatch::DispatchResult {
-        
-        // ---- We check the transaction is signed by the caller
-        // and retrieve the T::AccountId pubkey information.
-        let caller = ensure_signed(origin)?;
-        debug::info!("--- Called remove_stake with {:?}, hotkey {:?} and ammount {:?}", caller, hotkey, ammount_unstaked);
-
-        // ---- We query the Neuron set for the NeuronMetadata stored under
-        // the passed hotkey.
-        ensure!(Neurons::<T>::contains_key(&hotkey), Error::<T>::NotActive);
-        let neuron: NeuronMetadataOf<T> = Neurons::<T>::get(&hotkey);
-        debug::info!("Got metadata for hotkey.");
-
-        // ---- We check that the NeuronMetadata is linked to the calling
-        // cold key, otherwise throw a NonAssociatedColdKey error.
-        ensure!(neuron.coldkey == caller, Error::<T>::NonAssociatedColdKey);
-
-        // --- We call the emit function for the associated hotkey.
-        // Neurons must call an emit before they remove
-        // stake or they may be able to cheat their peers of inflation.
-        Self::emit_from_uid( neuron.uid );
-
-        // ---- We check that the hotkey has enough stake to withdraw
-        // and then withdraw from the account.
+    fn has_enough_stake(neuron : &NeuronMetadataOf<T>, amount : u64) -> bool{
         let hotkey_stake: u64 = Stake::get( neuron.uid );
-        ensure!(hotkey_stake >= ammount_unstaked, Error::<T>::NotEnoughStaketoWithdraw);
-        Stake::insert( neuron.uid, hotkey_stake - ammount_unstaked );
-        debug::info!("Withdraw: {:?} from hotkey staking account for new ammount {:?} staked", ammount_unstaked, hotkey_stake - ammount_unstaked);
-    
-        // --- We perform the withdrawl by converting the stake to a u64 balance
-        // and deposit the balance into the coldkey account. If the coldkey account
-        // does not exist it is created.
-        let ammount_unstaked_as_currency = Self::u64_to_balance( ammount_unstaked );
-        ensure!(ammount_unstaked_as_currency.is_some(), Error::<T>::CouldNotConvertToBalance);
-        let _ = T::Currency::deposit_creating(&caller, ammount_unstaked_as_currency.unwrap());
-        debug::info!("Deposit {:?} into coldkey balance ", ammount_unstaked_as_currency.unwrap());
-
-        // --- We update the total staking pool with the removed funds.
-        let total_stake: u64 = TotalStake::get();
-        TotalStake::put(total_stake - ammount_unstaked);
-        debug::info!("Remove {:?} from total stake, now {:?} ", ammount_unstaked, TotalStake::get());
-
-        // ---- Emit the unstaking event.
-        Self::deposit_event(RawEvent::StakeRemoved(hotkey, ammount_unstaked));
-        debug::info!("--- Done remove_stake.");
-        
-        // --- Done and ok.
-        Ok(())
+        return hotkey_stake >= amount;
     }
 }
+
