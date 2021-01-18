3cd48d1cb8b2e54aec4e07a6b3880d7b886efdfd
Parallax <parall4x@users.noreply.github.com>
2021-01-18 15:28:39 -0500

    - Updated version number v1.0.2

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -105,7 +105,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node-subtensor-runtime"), // Don't change this. It'll fuck up chain upgrades
 	impl_name: create_runtime_str!("node-subtensor-client"), // I guess the same goes for this
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 2,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
