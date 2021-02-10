cfed6e4cc7850c4ae32d467181e29f1537943f20
Parallax <parall4x@users.noreply.github.com>
2021-02-10 09:58:16 -0500

    - Added pallet-transaction-payment - This works with the bittensor subtensor client integration tests.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -353,7 +353,8 @@ pub type SignedExtra = (
 	frame_system::CheckEra<Runtime>,
 	frame_system::CheckNonce<Runtime>,
 	frame_system::CheckWeight<Runtime>,
-	pallet_transaction_payment::ChargeTransactionPayment<Runtime>
+	pallet_transaction_payment::ChargeTransactionPaymentOld<Runtime>,
+	pallet_subtensor::ChargeTransactionPayment<Runtime>
 );
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signature, SignedExtra>;
