6e1791f3740580bf3da2c34fb685cf76ca7ae324
Parallax <parall4x@users.noreply.github.com>
2021-04-01 13:01:06 -0500

    Fixed bug in test script

--- ci/script.sh
+++ ci/script.sh
@@ -12,7 +12,7 @@ set -ex
 #    fi
 
 #    cross test --target $TARGET
-    cross test --target $TARGET --release
+#    cross test --target $TARGET --release
 
 #    cross run --target $TARGET
 #    cross run --target $TARGET --release
